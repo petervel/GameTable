d2be7225ee6882ba8a5f412c3986dd1681381e5a
Peter Vel <petervel@gmail.com>
2019-11-22 15:22:06 +0100

    some cleanup

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-coffee -c -o js script
-cp style/* css/
+mkdir -p output
+cp src/static/* output/
+mkdir -p output/js
+coffee -c -o output/js src/script
+mkdir -p output/css
+cp src/style/* output/css/
